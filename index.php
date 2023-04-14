7ee77f37706b3b5120ceee8e9249ccf3b22402d6
sg.jiri <sg.jiri@yahoo.fr>
2023-04-14 16:52:57 +0200

    first comic

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <figure>
+            <img src="./Img/logo2.png" alt="logo">
+        </figure>
+        <nav>
+            <ul>
+                <li><a href="">Accueil</a></li>
+                <li><a href="">Mes realisation</a></li>
+                <li><a href="">Qui suis-je</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+
+        <figure id="burger">
+            <img src="./Img/burger-bar.png" alt="menu burger">
+
+        </figure>
+
+    </header>
+    <main>
+        <div id="sky">
+            <div id="clouds">
+                <figure id ="cloud1">
+                    <img src="./Img/Nouage1.svg" alt="cloud1">
+                </figure>
+                <figure id ="cloud2">
+                    <img src="./Img/Nuage 2.svg" alt="cloud2">
+                </figure>
+                <figure id ="cloud3">
+                    <img src="./Img/Nouage3.svg" alt="cloud3">
+                </figure>
+
+            </div>
+            <div id="imgPecheur">
+                <figure id="pecheur">
+                    <img src="./Img/pecheur.svg" alt="pecheur">
+                </figure>
+                <figure id="barque">
+                    <img src="./Img/barque 2.svg" alt="barque">
+                </figure>
+            </div>
+
+        </div>
+        <figure id="sea">
+            <img src="./Img/sea.svg" alt="sea">
+        </figure>
+    </main>
+    <!-- <script src="./main.js"></script> -->
+
+</body>
+
+</html>
