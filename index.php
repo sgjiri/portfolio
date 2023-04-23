e3205b6ce54d8745fb7b600eb933a76df8dd0e3d
sg.jiri <sg.jiri@yahoo.fr>
2023-04-23 14:20:28 +0200

    text

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Averia+Gruesa+Libre&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 
@@ -32,13 +35,13 @@
     <main>
         <div id="sky">
             <div id="clouds">
-                <figure id ="cloud1">
+                <figure id="cloud1">
                     <img src="./Img/Nouage1.svg" alt="cloud1">
                 </figure>
-                <figure id ="cloud2">
+                <figure id="cloud2">
                     <img src="./Img/Nuage 2.svg" alt="cloud2">
                 </figure>
-                <figure id ="cloud3">
+                <figure id="cloud3">
                     <img src="./Img/Nouage3.svg" alt="cloud3">
                 </figure>
 
@@ -57,10 +60,21 @@
             <figure id="reeds2">
                 <img src="./Img/raoseaux2.png" alt="reeds">
             </figure>
+            <div id="text">
+                <h1>Jirka Fischer</h1>
+                <h2>Developpeur web</h2>
+                
+
+
+            </div>
+
 
         </div>
         <figure id="sea">
             <img src="./Img/sea.svg" alt="sea">
+            <p>
+                Issue de l’agroalimentaire et de la théologie je suis un professionnel polyvalent, engagé et creatif, pret à apporter une contribution significative à tout projet.
+                </p>
         </figure>
     </main>
     <!-- <script src="./main.js"></script> -->
